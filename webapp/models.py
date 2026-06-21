477b55426926040eb857f5402c97309e605f7935
alexiadecosta <decostaalexia@gmail.com>
2026-06-21 09:09:35 -0400

    removed version declaration from docker-compose.yml and modularized initializing the database in create_app function

--- webapp/models.py
+++ webapp/models.py
@@ -2,6 +2,8 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from webapp import db, login_manager
 
+user_interests=db.Table('user_interests', db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True), db.Column('interest_id', db.Integer, db.ForeignKey('interests.id'), primary_key=True))
+resource_tags=db.Table('resource_tags', db.Column('resource_id', db.Integer, db.ForeignKey('resources.id'), primary_key=True), db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True))
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -10,6 +12,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    interests = db.relationship('Interest', secondary=user_interests, backref=db.backref('users', lazy='dynamic'))
+    saved_resources = db.relationship('Resource', secondary='saved_resources', backref=db.backref('saved_by_users', lazy='dynamic'))
     
     def set_password(self, password):
         """Hash and set the password"""
@@ -21,6 +25,36 @@ class User(UserMixin, db.Model):
     
     def __repr__(self):
         return f'<User {self.username}>'
+
+class Interest(db.Model):
+    __tablename__ = 'interests'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return f'<Interest {self.name}>'
+
+class Resource(db.Model):
+    __tablename__ = 'resources'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text)
+    url = db.Column(db.String(200), nullable=False)
+    tags = db.relationship('Tag', secondary=resource_tags, backref=db.backref('resources', lazy='dynamic'))
+
+    def __repr__(self):
+        return f'<Resource {self.title}>'
+    
+class SavedResource(db.Model):
+    __tablename__ = 'saved_resources'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    resource_id = db.Column(db.Integer, db.ForeignKey('resources.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('saved_resources_assoc', lazy='dynamic'))
+    resource = db.relationship('Resource', backref=db.backref('saved_resources_assoc', lazy='dynamic'))
 
 
 @login_manager.user_loader

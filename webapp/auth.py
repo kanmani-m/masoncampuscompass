517f2bda29a1551c79fda098daf56060ffe50418
alexiadecosta <decostaalexia@gmail.com>
2026-06-22 11:20:47 -0400

    interests form fix

--- webapp/auth.py
+++ webapp/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from webapp import db
-from webapp.models import User
+from webapp.models import User, Interest
 from webapp.forms import SignUpForm, LoginForm
 
 auth_bp = Blueprint('auth', __name__)
